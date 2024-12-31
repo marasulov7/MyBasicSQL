3aa8a2922b19824aea07bad50c2abec86dc4cd87
marasulov7 <marasulovalish@gmail.com>
2024-12-31 14:25:45 +0500

    Create VIEW vw_goods_categories

--- Views.sql
+++ Views.sql
@@ -1,6 +1,7 @@
 CREATE VIEW vw_goods AS
   SELECT g.id_goods, g.id_categories, g.goods, g.rest_in_stock
     FROM `goods` AS g
-    
 
-    
+CREATE VIEW vw_goods_categories AS
+  SELECT gc.id_categories, gc.categories_name, gc.categories_description
+    FROM `goods_categories` AS gc
