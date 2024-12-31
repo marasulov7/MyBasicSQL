37990fc410f88655728351b022eabfedeb16e969
marasulov7 <marasulovalish@gmail.com>
2024-12-31 14:35:13 +0500

    Count goods quantity using INNER JOIN

--- Views.sql
+++ Views.sql
@@ -5,3 +5,16 @@ CREATE VIEW vw_goods AS
 CREATE VIEW vw_goods_categories AS
   SELECT gc.id_categories, gc.categories_name, gc.categories_description
     FROM `goods_categories` AS gc
+
+SELECT c.categories_name,
+  COUNT(g.id_goods) AS goods_cnt
+  FROM 
+    vw_goods AS g
+  INNER JOIN 
+    vw_goods_categories AS c 
+  ON
+    g.id_categories = c.id_categories
+  GROUP BY 
+    c.categories_name
+  ORDER BY
+    c.categories_name;  
